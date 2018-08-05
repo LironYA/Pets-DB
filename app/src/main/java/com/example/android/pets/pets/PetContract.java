d449a45728aa8c3b9d7ca9867b7f7fef71a7b59e
LironYA <korkinazor@gmail.com>
2018-08-05 20:43:14 +0300

    ContentProvider Changes

--- app/src/main/java/com/example/android/pets/pets/PetContract.java
+++ app/src/main/java/com/example/android/pets/pets/PetContract.java
@@ -1,9 +1,17 @@
 package com.example.android.pets.pets;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 //Class is final because it's just a class to providing constants and there no need to import or extend
 public final class PetContract {
+    // we set this up as a string constant whose value is the same as that from the AndroidManifest
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+    //we concatenate the CONTENT_AUTHORITY constant with the scheme “content://” we will create the BASE_CONTENT_URI which will be shared by every URI associated with PetContract:
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    //This constants stores the path for each of the tables which will be appended to the base content URI.
+    public static final String PATH_PETS = "pets";
+
 
     public static abstract class PetEntry implements BaseColumns {
         public static final String TABLE_NAME = "pets";
@@ -12,6 +20,9 @@ public final class PetContract {
         public final static String COLUMN_PET_BREED = "breed";
         public final static String COLUMN_PET_GENDER = "gender";
         public final static String COLUMN_PET_WEIGHT = "weight";
+        //inside each of the Entry classes in the contract, we create a full URI for the class as a constant called CONTENT_URI.
+        // The Uri.withAppendedPath() method appends the BASE_CONTENT_URI (which contains the scheme and the content authority) to the path segment.
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         // Possible values
 
